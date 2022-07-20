2ef72b0aa9d9c46d69b780b2df647242cf12191b
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-07-19 21:10:18 -0400

    added audio to player losing added "you lose" Dark_Souls_You_Died_Screen img

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+
 var character = document.getElementById('player')
 
 function jump() {
 
     if (character.classList != 'animate') {
         character.classList.add('animate')
-    } 
+    }
     setTimeout(function () {
         character.classList.remove('animate')
 
@@ -18,10 +19,19 @@ var enemy = document.getElementById('enemy')
 var lose = setInterval(function () {
     var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
     var enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue('left'));
-    if(enemyLeft<5 && enemyLeft>0 && playerTop>=300 ){
-        enemy.style.animation = "none";
-        enemy.style.display = "none";
-        alert("you died")
+    if (enemyLeft < 5 && enemyLeft > 0 && playerTop >= 300) {
+        document.getElementById('gameDiv').innerHTML = deathSound.play()
+        document.getElementById('gameDiv').innerHTML = '<img width="750" height="350" src="./assets/Dark_Souls_You_Died_Screen_-_Completely_Black_Screen_0-2_screenshot.png">';
+        
     }
-    
+
+
+
+
 })
+
+var deathSound = new Audio('./assets/You Died.mp3');
+deathSound.volume = .05
+
+var youDied = document.createElement('img')
+
