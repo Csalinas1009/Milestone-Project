2bb922aa50036bff556cd49f71a4d2335d3cff6e
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-07-21 17:48:19 -0400

    added alert message to show what player score was at the end of the game

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+//define player
 var character = document.getElementById('player')
+
+//define enemy npc
 var enemy = document.getElementById('enemy')
+
+
+//define audio on death and audio while playing
 var deathSound = new Audio('./assets/You Died.mp3');
-deathSound.volume = .05
-var youDied = document.createElement('img')
+deathSound.volume = .90
 
 
+//reset button when game ends
 const resetButton = document.getElementById('resetButton');
 
 resetButton.addEventListener('click', () => {
     location.reload()
 })
 
+
+//score counter
 var score = 0
 
 window.onload = function () {
@@ -21,7 +29,7 @@ window.onload = function () {
         scoreLabel.innerHTML = "Score " + score;
     }, 75)
 }
-
+//jump function for player
 function jump() {
 
     if (character.classList != 'animate') {
@@ -31,13 +39,15 @@ function jump() {
         character.classList.remove('animate')
     }, 500);
 }
+
+//lose conditions
 var lose = setInterval(function () {
     var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
     var enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue('left'));
     if (enemyLeft < 5 && enemyLeft > 0 && playerTop >= 300) {
         alert('Your score is ' + score + '!')
         document.getElementById('scoring')
-        scoring.remove();
+        scoring.remove()
         document.getElementById('gameDiv').innerHTML = deathSound.play()
         document.getElementById('gameDiv').innerHTML = '<img width="750" height="350" src="./assets/Dark_Souls_You_Died_Screen_-_Completely_Black_Screen_0-2_screenshot.png">';
     }
